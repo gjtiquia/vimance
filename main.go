d08ad2f88c943e6d49fbeafc172987e13ef6e999
GJ Tiquia <gjtiquia@gmail.com>
2026-03-22 23:18:38 +0800

    add TODOs

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	"github.com/gjtiquia/vimance/web/components"
 )
 
+// TODO :
+// - setup air
+// - setup hot reload
+// - setup tailwind
+// - setup layout_base.templ and page_home.templ
+// - setup spreadsheet visual
+// - setup vim keybinds (keep in mind mouse and mobile controls as well)
+// - keep in mind modality, undo tree, keybinding config, command palette
+// - command mode :w sends to server with visual indicator (disable when saving)
+// - htmx, server side sqlite setup
+// - special columns handling (dates, tags, auto-complete behavior)
+
 func main() {
 	http.Handle("/", templ.Handler(components.HomePage()))
 
