b58bc96570dd64466e27b3e83a1e9465adcf81a6
GJ Tiquia <gjtiquia@gmail.com>
2026-03-24 09:04:40 +0800

    wip mode switch

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,10 +1,10 @@
 package engine
 
 type Engine struct {
-	Mode Mode
+	mode Mode
 }
 
-type Mode uint 
+type Mode uint
 
 const (
 	ModeNormal Mode = iota
@@ -12,8 +12,39 @@ const (
 	ModeVisual
 )
 
+// RuneEsc is the rune value for the Escape key.
+const RuneEsc rune = '\x1b'
+
 func New() Engine {
 	return Engine{
-		Mode: ModeNormal,
+		mode: ModeNormal,
+	}
+}
+
+func (eng *Engine) Mode() Mode {
+	return eng.mode
+}
+
+// TODO : consider special keys and likely need to change to something else other than runes
+// TODO : perhaps just strings for simplicity
+func (eng *Engine) KeyPress(key rune) {
+	switch eng.mode {
+
+	case ModeNormal:
+		if key == 'i' {
+			eng.mode = ModeInsert
+		} else if key == 'v' {
+			eng.mode = ModeVisual
+		}
+
+	case ModeInsert:
+		if key == RuneEsc { // Escape key
+			eng.mode = ModeNormal
+		}
+
+	case ModeVisual:
+		if key == RuneEsc { // Escape key
+			eng.mode = ModeNormal
+		}
 	}
 }
--- internal/engine/engine_test.go
+++ internal/engine/engine_test.go
@@ -9,7 +9,35 @@ import (
 func TestInitialState(t *testing.T) {
 	eng := engine.New()
 
-	if eng.Mode != engine.ModeNormal {
-		t.Errorf("expected initial mode to be normal, got %v", eng.Mode)
+	if eng.Mode() != engine.ModeNormal {
+		t.Errorf("expected initial mode to be normal, got %v", eng.Mode())
+	}
+}
+
+func TestModeSwitching(t *testing.T) {
+	eng := engine.New()
+
+	// Switch to insert mode
+	eng.KeyPress('i')
+	if eng.Mode() != engine.ModeInsert {
+		t.Fatalf("expected mode to be insert, got %v", eng.Mode())
+	}
+
+	// Switch back to normal mode
+	eng.KeyPress(engine.RuneEsc) // Escape key
+	if eng.Mode() != engine.ModeNormal {
+		t.Fatalf("expected mode to be normal, got %v", eng.Mode())
+	}
+
+	// Switch to visual mode
+	eng.KeyPress('v')
+	if eng.Mode() != engine.ModeVisual {
+		t.Fatalf("expected mode to be visual, got %v", eng.Mode())
+	}
+
+	// Switch back to normal mode
+	eng.KeyPress(engine.RuneEsc) // Escape key
+	if eng.Mode() != engine.ModeNormal {
+		t.Fatalf("expected mode to be normal, got %v", eng.Mode())
 	}
 }
