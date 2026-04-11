2b76a2d9a214a3915db39e5d526d7d30dc4fb33f
GJ Tiquia <gjtiquia@gmail.com>
2026-04-11 19:24:35 +0800

    custom keybindings

--- list.go
+++ list.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"strings"
 
+	"charm.land/bubbles/v2/key"
 	"charm.land/bubbles/v2/list"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
@@ -33,6 +34,9 @@ func NewUnstyledList(items []list.Item) list.Model {
 	// show all by default
 	l.Help.ShowAll = true
 	// TODO : should... customize this...?
+	// TODO : see list.KeyMap
+
+	l.KeyMap = CustomKeyMap()
 
 	l.FilterInput.Prompt = "type command: "
 
@@ -57,28 +61,27 @@ func (m Model) UpdateListInput(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		case "esc":
-			if m.userListInput.FilterState() != list.Filtering {
+			if m.userListInput.FilterState() == list.Filtering {
+				// this is still needed for the case of empty filter text
+				m.userListInput.SetFilterState(list.FilterApplied)
+			} else {
 				return m, tea.Quit
-			}
-
-		// TODO : keybindings (like j and k handling!)
-		case "i", "a":
-			if m.userListInput.FilterState() != list.Filtering {
-				m.userListInput.SetFilterText("")
-				m.userListInput.SetFilterState(list.Filtering)
-				return m, nil
 			}
 
 		case "enter":
 			// always submit even if in filtering state
 
-			item := m.userListInput.Items()[m.userListInput.GlobalIndex()].(ListItem)
+			visibleItems := m.userListInput.VisibleItems()
+			visibleIndex := m.userListInput.Index()
+			if len(visibleItems) > 0 {
+				item := visibleItems[visibleIndex].(ListItem)
 
-			itemRender := m.userTextInput.Prompt + string(item.title) + "\n"
-			m.history = append(m.history, itemRender)
+				itemRender := m.userTextInput.Prompt + string(item.title) + "\n"
+				m.history = append(m.history, itemRender)
 
-			// TODO : for now, swap between
-			return m.EnterTextInput(), nil
+				// TODO : for now, swap between
+				return m.EnterTextInput(), nil
+			}
 		}
 	}
 
@@ -123,4 +126,72 @@ func (d ListItemDelegate) Render(w io.Writer, m list.Model, index int, listItem 
 	str += fmt.Sprintf("%s    %s\n", cursor, item.desc)
 	str += fmt.Sprintf("%s    alias: [%s]\n", cursor, strings.Join(item.alias, ", "))
 	fmt.Fprint(w, str)
+}
+
+func CustomKeyMap() list.KeyMap {
+	return list.KeyMap{
+		// Browsing.
+		CursorUp: key.NewBinding(
+			key.WithKeys("up", "k"),
+			key.WithHelp("↑/k", "up"),
+		),
+		CursorDown: key.NewBinding(
+			key.WithKeys("down", "j"),
+			key.WithHelp("↓/j", "down"),
+		),
+
+		PrevPage: key.NewBinding(
+			key.WithKeys("left", "h", "pgup"),
+			key.WithHelp("←/h/pgup", "prev page"),
+		),
+		NextPage: key.NewBinding(
+			key.WithKeys("right", "l", "pgdown"),
+			key.WithHelp("→/l/pgdn", "next page"),
+		),
+
+		GoToStart: key.NewBinding(
+			key.WithKeys("home", "g"),
+			key.WithHelp("g", "go to start"),
+		),
+		GoToEnd: key.NewBinding(
+			key.WithKeys("end", "G"),
+			key.WithHelp("G", "go to end"),
+		),
+
+		Filter: key.NewBinding(
+			key.WithKeys("/", "ctrl+f", "i", "a"),
+			key.WithHelp("ctrl+f/i/a", "filter"),
+		),
+
+		ClearFilter: key.Binding{},
+
+		// Filtering.
+		CancelWhileFiltering: key.NewBinding(
+			key.WithKeys(""),
+			key.WithHelp("↑/↓", "up/down"), // this is for visualization purposes only
+		),
+
+		AcceptWhileFiltering: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("enter", "select"), // this is for visualization purposes only
+			// TODO : this help should be shown always, as well as esc
+		),
+
+		// Toggle help.
+		ShowFullHelp: key.NewBinding(
+			key.WithKeys("?"),
+			key.WithHelp("?", "more"),
+		),
+		CloseFullHelp: key.NewBinding(
+			key.WithKeys("?"),
+			key.WithHelp("?", "close help"),
+		),
+
+		// Quitting.
+		Quit: key.NewBinding(
+			key.WithKeys("q"),
+			key.WithHelp("q", "quit"),
+		),
+		ForceQuit: key.NewBinding(key.WithKeys("ctrl+c")),
+	}
 }
